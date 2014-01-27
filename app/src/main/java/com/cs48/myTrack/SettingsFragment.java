0df185b97d2b72554b35c7c86f67a6cf1ce6c245
dcoffill <decoffill@gmail.com>
2014-01-26 21:46:32 -0800

    Added partially-implemented settings menu (third time's the charm...)

--- app/src/main/java/com/cs48/myTrack/SettingsFragment.java
+++ app/src/main/java/com/cs48/myTrack/SettingsFragment.java
@@ -1,7 +1,20 @@
 package com.cs48.myTrack;
 
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
 /**
  * Created by david on 1/26/14.
  */
-public class SettingsFragment {
+
+/* really simple fragment code that creates and Android Preferences Fragment on loading
+ */
+public class SettingsFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.preferences);
+    }
 }
