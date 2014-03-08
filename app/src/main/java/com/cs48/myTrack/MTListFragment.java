9ad07b5d3daf8f7fa8a9e2350e67702cb5fe2a43
hzpkk520 <hzpkk520@gmail.com>
2014-03-07 18:39:12 -0800

    Add a few lines to store item numbers selected for popupDialog to use

--- app/src/main/java/com/cs48/myTrack/MTListFragment.java
+++ app/src/main/java/com/cs48/myTrack/MTListFragment.java
@@ -24,29 +24,40 @@ public class MTListFragment extends ListFragment{
         //super.onListItemClick(l, v, pos, id);
         //Toast.makeText(getActivity(), "Please add location description", Toast.LENGTH_SHORT).show();
 
+        //create a DatabaseHelper to be used
+        DatabaseHelper dbHelper = new DatabaseHelper(getActivity());
 
         //give the time of selected item to search for corresponding location item in the database
         //convert date and time to milliseconds first
         String[] splitString = ((String)(getListView().getItemAtPosition(pos))).split(":",2);
         //Format: 2014-03-06 14:15:35
         String givenDateString = splitString[1];
+
 //        Log.e("SplitString","The split string is:" + givenDateString);
         SimpleDateFormat sdf = new SimpleDateFormat(" yyyy-MM-dd HH:mm:ss");
         try {
             Date mDate = sdf.parse(givenDateString);
             long timeInMilliseconds = mDate.getTime();
 //            Log.e("timeInMilli","Time in Milli is: "+timeInMilliseconds);
-            //search for the item and store its description in DescriptionText.description
-            DatabaseHelper dbHelper = new DatabaseHelper(getActivity());
+            //search for the item and store its description in ListDialogTransactor.description
             LocationInfo tmpLocationInfo = dbHelper.getLocationByTime(timeInMilliseconds);
             String tmpDescription = tmpLocationInfo.get_Description();
             if (tmpDescription!=null){
-                DescriptionText.description = tmpDescription;
+                ListDialogTransactor.description = tmpDescription;
             }
+            //store the item get ListDialogTransactor.locationInfo
+            ListDialogTransactor.locationInfo = tmpLocationInfo;
+
+            //raw string to get item num
+            String givenItemNumPart = splitString[0];
+            ListDialogTransactor.itemNum = Integer.parseInt(givenItemNumPart.substring(10,(givenItemNumPart.length())));
 
         }catch (ParseException e) {
             e.printStackTrace();
         }
+
+
+
 
 
 
