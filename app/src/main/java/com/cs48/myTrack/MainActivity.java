f845ef452952b35d5218bda6b60c4a121c0a7bc4
hzpkk520 <hzpkk520@gmail.com>
2014-03-09 16:59:16 -0700

    Deleted unnecessary imports.

--- app/src/main/java/com/cs48/myTrack/MainActivity.java
+++ app/src/main/java/com/cs48/myTrack/MainActivity.java
@@ -17,7 +17,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,12 +24,6 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.location.LocationClient;
-
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
 
 
 public class MainActivity extends Activity implements
